e3318a79da51e6f8e04c38fa8f1b757f2477daf8
Louise <63709507+Louise0709@users.noreply.github.com>
2020-04-28 13:41:47 +0800

    Update card.py

--- Code/card.py
+++ Code/card.py
@@ -1,13 +1,15 @@
 import random
 card_turple=('Spiderman','Ironman','Loki','Thor','Cap')
-package=[]
+package={}
+weight=(5,10,50,50,100)
+comapare=(15,35,115,215)
 while 1:	
 	choice=int(input('1.draw a card\n 2.look into the bag\n 3.arrange the bag\n 4.leave.\n Which number do you choose?'))
 
 	if choice ==1:
 		num =int(input('how many times?'))
 		for i in range(0,num):
-			n= random.randint(0,5)
+			n= random.randint(1,weight[-1])
 			print('you got{}'.format(card_turple[n]))
 			package.append(card_turple[n])
 			print('put {} in in the bag already'.format(card_turple[n]))		
